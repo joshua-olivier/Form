3c1d2001cd043dbb437a455c0238f09f917da3e9
Joshua Olivier <108597396+joshua-olivier@users.noreply.github.com>
2022-10-22 20:22:48 -0400

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,2 @@
+window.alert("This is a mini form with some simple questions for you to answer!")
+window.alert("If you have any inquiries, send me an email: joshuaolivierr@gmail.com. I would be happy to respond!")
